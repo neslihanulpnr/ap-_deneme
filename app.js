ef46b6d582190b629b198db7d7cf5424ac56af2c
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-11-29 10:23:44 +0300

    new

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+// 1.
+// fetch("setting.json").then(
+ //   response => {
+    //console.log(response)
+    // }
+ // )
+
+
+ //2.
+//fetch("setting.json").then(
+    //response => {
+        //return response.json()
+    //}
+//).then(responseJson => {
+   //console.log(responseJson)
+//})
+
+
+//3.
 fetch("setting.json").then(
-    response => {
-        return response.json()
-    }
-).then(responseJson => {
-   console.log(responseJson)
+    response => response.json()
+).then (responseJson => {
+    console.log(responseJson)
+    console.log(responseJson.userName)
 })
