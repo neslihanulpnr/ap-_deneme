d701641753cd51e2e6505cc3a3c146139f1d44eb
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-11-29 10:59:43 +0300

    new

--- app.js
+++ app.js
@@ -16,10 +16,32 @@
 //})
 
 
-//3.
+//3. json dosyadan veri çekmek
 fetch("setting.json").then(
     response => response.json()
 ).then(responseJson => {
     console.log(responseJson)
     console.log(responseJson.userName)
+})
+
+
+//4. apı üzerinden veri çekmek
+//fetch("https://jsonplaceholder.typicode.com/posts").then(
+  //  response => response.json()
+//).then(responseJson => {
+   //responseJson.forEach(item => console.log(item));
+//})
+
+
+
+let userListDOM = document.querySelector("#userList") 
+//5. apı üzerinden veri çekmek
+fetch("https://jsonplaceholder.typicode.com/posts").then(
+    response => response.json()
+).then(responseJson => {
+   responseJson.forEach(item => {
+    let liDOM = document.createElement("li")
+    liDOM.innerHTML = item.title
+    userListDOM.appendChild(liDOM)
+   });
 })
